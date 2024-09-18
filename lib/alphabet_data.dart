6a5e8413753a4e0af1a175563cb079f9a7367770
risesia <rizkyterm@gmail.com>
2024-09-18 21:41:41 +0700

    first commit

---
+++ lib/alphabet_data.dart
@@ -0,0 +1,161 @@
+const List<Map<String, String>> consonants = [
+  {
+    'letter': 'B',
+    'audio': 'b',
+    'example1': 'banane',
+    'ipa1': 'ba.nan',
+    'translation1': 'pisang',
+    'example2': 'bateau',
+    'ipa2': 'bato',
+    'translation2': 'kapal'
+  },
+  {
+    'letter': 'D',
+    'audio': 'd',
+    'example1': 'dame',
+    'ipa1': 'dam',
+    'translation1': 'wanita',
+    'example2': 'radio',
+    'ipa2': 'radjo',
+    'translation2': 'radio'
+  },
+  {
+    'letter': 'F',
+    'audio': 'f',
+    'example1': 'famille',
+    'ipa1': 'famij',
+    'translation1': 'keluarga',
+    'example2': 'fille',
+    'ipa2': 'fij',
+    'translation2': 'anak perempuan'
+  },
+  {
+    'letter': 'G',
+    'audio': 'g',
+    'example1': 'garçon',
+    'ipa1': 'gaʁ.sɔ̃',
+    'translation1': 'anak laki-laki',
+    'example2': 'goût',
+    'ipa2': 'gu',
+    'translation2': 'mencicipi'
+  },
+  {
+    'letter': 'K',
+    'audio': 'k',
+    'example1': 'carte',
+    'ipa1': 'kaʁt',
+    'translation1': 'kartu',
+    'example2': 'coucou',
+    'ipa2': 'kuku',
+    'translation2': 'hai'
+  },
+  {
+    'letter': 'ʃ',
+    'audio': 'ch',
+    'example1': 'chat',
+    'ipa1': 'ʃa',
+    'translation1': 'kucing',
+    'example2': 'chou',
+    'ipa2': 'ʃu',
+    'translation2': 'kubis'
+  },
+  {
+    'letter': 'L',
+    'audio': 'l',
+    'example1': 'lampe',
+    'ipa1': 'lɑ̃p',
+    'translation1': 'lampu',
+    'example2': 'long',
+    'ipa2': 'lɔ̃',
+    'translation2': 'panjang'
+  },
+  {
+    'letter': 'M',
+    'audio': 'm',
+    'example1': 'mais',
+    'ipa1': 'mɛ',
+    'translation1': 'tetapi',
+    'example2': 'Maman',
+    'ipa2': 'ma.mɑ̃',
+    'translation2': 'mama'
+  },
+];
+
+const List<Map<String, String>> vowels = [
+  {
+    'letter': 'A',
+    'audio': 'a',
+    'example1': 'Avion',
+    'ipa1': 'avjɔ̃',
+    'translation1': 'Pesawat',
+    'example2': 'Arbre',
+    'ipa2': 'aʁbʁ',
+    'translation2': 'Pesawat'
+  },
+  {
+    'letter': 'E',
+    'audio': 'e',
+    'example1': 'École',
+    'ipa1': 'ekɔl',
+    'translation1': 'Halo',
+    'example2': 'Eau',
+    'ipa2': 'o',
+    'translation2': 'Sekolah'
+  },
+];
+
+const List<Map<String, String>> semiConsonants = [
+  {
+    'letter': 'J',
+    'audio': 'j',
+    'example1': 'famille',
+    'ipa1': 'famij',
+    'translation1': 'keluarga',
+    'example2': 'travail',
+    'ipa2': 'tʁa.vɑj',
+    'translation2': 'pekerjaan'
+  },
+  {
+    'letter': 'W',
+    'audio': 'w',
+    'example1': 'bois',
+    'ipa1': 'bwa',
+    'translation1': 'kayu',
+    'example2': 'oui',
+    'ipa2': 'wi',
+    'translation2': 'iya'
+  },
+  {
+    'letter': 'ɥ',
+    'audio': 'ɥ',
+    'example1': 'huile',
+    'ipa1': 'ɥil',
+    'translation1': 'minyak',
+    'example2': 'lune',
+    'ipa2': 'lyn',
+    'translation2': 'bulan'
+  },
+];
+
+const List<Map<String, String>> vokalOral = [
+  {
+    'letter': 'ɑ̃',
+    'audio': 'ɑ̃',
+    'example1': 'gens',
+    'ipa1': 'ʒɑ̃',
+    'translation1': 'orang',
+    'example2': 'grand',
+    'ipa2': 'gRɑ̃',
+    'translation2': 'besar'
+  },
+  {
+    'letter': 'ɛ̃',
+    'audio': 'ɛ̃',
+    'example1': 'pain',
+    'ipa1': 'pɛ̃',
+    'translation1': 'roti',
+    'example2': 'vin',
+    'ipa2': 'vɛ̃',
+    'translation2': 'anggur'
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'alphabet_data.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+// TODO:
+// homepage,
+// gambar untuk example,
+// tambah data baru,
+// redesign, include warna merah,
+// contoh yang bold di docs, tetap bold di app
+// liason ditandai dengan warna yangsama, berdasarkan uruf bold, ditambahkan keterangan
+// pengelompokan liaison berdasarkan pertemuan huruf
+// pengelompokan kalimat dasar berdasarkan tema
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,7 +29,24 @@ class MyApp extends StatelessWidget {
         title: 'French Alphabet Dictionary',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 194, 226)),
+          scaffoldBackgroundColor:
+              Color.fromARGB(255, 251, 253, 255), // Set app background color
+          cardColor: Color(0xff317480), // Set card color
+          textTheme: TextTheme(
+            headlineLarge: TextStyle(
+                color: Color(0xff050608)), // Set text color for large headlines
+            headlineSmall: TextStyle(
+                color:
+                    Color(0xff050608)), // Set text color for smaller headlines
+            bodyMedium: TextStyle(
+                color: Color(0xff050608)), // Set text color for medium text
+            titleLarge: TextStyle(
+                color: Color(0xff050608)), // Set text color for title text
+            titleMedium: TextStyle(
+                color: Color(0xff050608)), // Set text color for subtitle text
+          ),
         ),
         home: MyHomePage(),
       ),
@@ -27,31 +55,45 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  // Alphabet data: letters, pronunciation audio URL, example, and translation
-  final consonants = [
-    {'letter': 'B', 'audio': 'assets/b.mp3', 'example': 'Bonjour', 'translation': 'Hello'},
-    {'letter': 'C', 'audio': 'assets/c.mp3', 'example': 'Chat', 'translation': 'Cat'},
-    // Add more consonants here
-  ];
+  void navigateToAlphabetPage(
+      BuildContext context, String letter, String category) {
+    var alphabetData;
 
-  final vowels = [
-    {'letter': 'A', 'audio': 'assets/a.mp3', 'example': 'Avion', 'translation': 'Plane'},
-    {'letter': 'E', 'audio': 'assets/e.mp3', 'example': 'École', 'translation': 'School'},
-    // Add more vowels here
-  ];
+    // Use category to determine which list to search
+    switch (category) {
+      case 'Consonants':
+        alphabetData =
+            consonants.firstWhere((element) => element['letter'] == letter);
+        break;
+      case 'Semi-Consonants':
+        alphabetData =
+            semiConsonants.firstWhere((element) => element['letter'] == letter);
+        break;
+      case 'Vowels':
+        alphabetData =
+            vowels.firstWhere((element) => element['letter'] == letter);
+        break;
+      case 'Vokal Oral':
+        alphabetData =
+            vokalOral.firstWhere((element) => element['letter'] == letter);
+        break;
+      default:
+        throw UnimplementedError('No data for category $category');
+    }
 
-  void navigateToAlphabetPage(BuildContext context, String letter, bool isConsonant) {
-    var alphabetData = isConsonant
-        ? consonants.firstWhere((element) => element['letter'] == letter)
-        : vowels.firstWhere((element) => element['letter'] == letter);
+    // Navigate to the detail page with the alphabet data
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => AlphabetDetailPage(
           letter: alphabetData['letter']!,
           audioUrl: alphabetData['audio']!,
-          example: alphabetData['example']!,
-          translation: alphabetData['translation']!,
+          example1: alphabetData['example1']!,
+          ipa1: alphabetData['ipa1']!,
+          translation1: alphabetData['translation1']!,
+          example2: alphabetData['example2']!,
+          ipa2: alphabetData['ipa2']!,
+          translation2: alphabetData['translation2']!,
         ),
       ),
     );
@@ -66,21 +108,55 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
 
+  // Method to get the current page title based on the selected index
+  String getPageTitle() {
+    switch (selectedIndex) {
+      case 0:
+        return 'Konsonan';
+      case 1:
+        return 'Semi-konsonan';
+      case 2:
+        return 'Vokal Oral';
+      case 3:
+        return 'Vokal Sengau';
+      default:
+        return 'French Alphabet Dictionary'; // Default title
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = AlphabetListPage(isConsonant: true);  // Consonants
+        page = AlphabetListPage(category: 'Consonants'); // Consonants
+        break;
       case 1:
-        page = AlphabetListPage(isConsonant: false);  // Vowels
+        page = AlphabetListPage(category: 'Semi-Consonants'); // Semi-consonants
+        break;
+      case 2:
+        page = AlphabetListPage(category: 'Vowels'); // Vowels
+        break;
+      case 3:
+        page = AlphabetListPage(category: 'Vokal Oral'); // Vokal Oral
+        break;
       default:
         throw UnimplementedError('No widget for index $selectedIndex');
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('French Alphabet Dictionary'),
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromRGBO(227, 251, 255, 1), Color(0xFFFBFDFF)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+        title: Text(getPageTitle()),
         actions: [
           IconButton(
             icon: Icon(Icons.qr_code_scanner),
@@ -91,106 +167,297 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Row(
-        children: [
-          NavigationRail(
-            destinations: [
-              NavigationRailDestination(
-                icon: Icon(Icons.text_fields),
-                label: Text('Consonants'),
+      drawer: Drawer(
+        // Hamburger menu for navigation
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Color(0xFF007ae2), Color.fromRGBO(0, 194, 226, 1)],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight)),
+              child: Text(
+                'Panduan Pelafalan Niveau A1',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
               ),
-              NavigationRailDestination(
-                icon: Icon(Icons.text_fields),
-                label: Text('Vowels'),
-              ),
-            ],
-            selectedIndex: selectedIndex,
-            onDestinationSelected: (value) {
-              setState(() {
-                selectedIndex = value;
-              });
-            },
-          ),
-          Expanded(child: page),
-        ],
+            ),
+            ListTile(
+              leading: Icon(Icons.text_fields),
+              title: Text('Konsonan'),
+              onTap: () {
+                setState(() {
+                  selectedIndex = 0;
+                });
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.text_fields),
+              title: Text('Semi-Konsonan'),
+              onTap: () {
+                setState(() {
+                  selectedIndex = 1;
+                });
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.text_fields),
+              title: Text('Vokal Oral'),
+              onTap: () {
+                setState(() {
+                  selectedIndex = 2;
+                });
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.text_fields),
+              title: Text('Vokal Sengau'),
+              onTap: () {
+                setState(() {
+                  selectedIndex = 3;
+                });
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
       ),
+      body: page, // Display the current page
     );
   }
 }
 
 class AlphabetListPage extends StatelessWidget {
-  final bool isConsonant;
-  
-  AlphabetListPage({required this.isConsonant});
+  final String category;
+
+  AlphabetListPage({required this.category});
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var alphabetList = isConsonant ? appState.consonants : appState.vowels;
+    var alphabetList;
 
-    return ListView.builder(
+    switch (category) {
+      case 'Consonants':
+        alphabetList = consonants;
+        break;
+      case 'Semi-Consonants':
+        alphabetList = semiConsonants;
+        break;
+      case 'Vowels':
+        alphabetList = vowels;
+        break;
+      case 'Vokal Oral':
+        alphabetList = vokalOral;
+        break;
+      default:
+        throw UnimplementedError('No list for category $category');
+    }
+
+    var appState = context.watch<MyAppState>();
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // Display 2 cards per row
+        crossAxisSpacing: 16, // Space between columns
+        mainAxisSpacing: 16, // Space between rows
+        childAspectRatio: 1.0, // Make cards square
+      ),
+      padding: const EdgeInsets.all(16),
       itemCount: alphabetList.length,
       itemBuilder: (context, index) {
         var alphabet = alphabetList[index];
-        return ListTile(
-          title: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(alphabet['letter']!,
-                  style: Theme.of(context).textTheme.titleLarge),
+        return GestureDetector(
+          onTap: () => appState.navigateToAlphabetPage(
+              context, alphabet['letter']!, category),
+          child: Card(
+            color: const Color.fromARGB(
+                255, 0, 122, 227), // Set card color explicitly
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  alphabet['letter']!,
+                  style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 255, 255), // Text color
+                      ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ),
-          onTap: () => appState.navigateToAlphabetPage(context, alphabet['letter']!, isConsonant),
         );
       },
     );
   }
 }
 
-class AlphabetDetailPage extends StatelessWidget {
+class AlphabetDetailPage extends StatefulWidget {
   final String letter;
-  final String audioUrl;
-  final String example;
-  final String translation;
+  final String audioUrl; // Change this to match the argument in navigateToAlphabetPage
+  final String example1;
+  final String ipa1;
+  final String translation1;
+  final String example2;
+  final String ipa2;
+  final String translation2;
 
   AlphabetDetailPage({
     required this.letter,
-    required this.audioUrl,
-    required this.example,
-    required this.translation,
+    required this.audioUrl, // Match with 'audioUrl'
+    required this.example1,
+    required this.ipa1,
+    required this.translation1,
+    required this.example2,
+    required this.ipa2,
+    required this.translation2,
   });
 
   @override
-  Widget build(BuildContext context) {
-    final player = AudioPlayer();
+  AlphabetDetailPageState createState() => AlphabetDetailPageState();
+}
 
+class AlphabetDetailPageState extends State<AlphabetDetailPage> {
+  late FlutterTts flutterTts;
+
+  @override
+  void initState() {
+    super.initState();
+    flutterTts = FlutterTts();
+
+    // Optionally configure the TTS language for French pronunciation
+    flutterTts.setLanguage('fr-FR'); // Set to French
+  }
+
+  Future<void> _speak(String text) async {
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Details: $letter')),
+      appBar: AppBar(
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromRGBO(227, 251, 255, 1), Color(0xFFFBFDFF)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+        title: Text('Details: ${widget.letter}'),
+        backgroundColor: Color(0xFF007ae2), // Custom color or gradient
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Letter display at the top
             Text(
-              'Pronunciation of $letter',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            IconButton(
-              icon: Icon(Icons.play_arrow),
-              onPressed: () async {
-                // Use UrlSource if the audio is from a URL, or AssetSource if it's a local asset.
-                await player.play(UrlSource(audioUrl)); 
-              },
+              widget.letter,
+              style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                    fontSize: 100,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                  ),
+              textAlign: TextAlign.center,
             ),
             SizedBox(height: 20),
-            Text('Example: $example'),
-            Text('Translation: $translation (in Indonesian)'),
+            // Play button for pronunciation
+            IconButton(
+              icon: Icon(Icons.play_circle, size: 50),
+              onPressed: () {
+                _speak(widget.audioUrl); // Play the pronunciation using audioUrl
+              },
+            ),
+            SizedBox(height: 30),
+            // Example 1 section
+            _buildExampleSection(
+              exampleText: widget.example1,
+              ipaText: widget.ipa1,
+              translationText: widget.translation1,
+              playExample: () => _speak(widget.example1),
+              exampleTitle: 'Example 1',
+            ),
+            SizedBox(height: 20),
+            // Example 2 section
+            _buildExampleSection(
+              exampleText: widget.example2,
+              ipaText: widget.ipa2,
+              translationText: widget.translation2,
+              playExample: () => _speak(widget.example2),
+              exampleTitle: 'Example 2',
+            ),
           ],
         ),
       ),
     );
   }
+
+  // Widget to build the example section
+  Widget _buildExampleSection({
+    required String exampleText,
+    required String ipaText,
+    required String translationText,
+    required VoidCallback playExample,
+    required String exampleTitle,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '$exampleTitle',
+          style: Theme.of(context).textTheme.labelLarge,
+        ),
+      Row(
+        children: [
+          Text(
+              '$exampleText',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            IconButton(
+              icon: Icon(Icons.play_circle),
+              onPressed: playExample,
+            ),
+        ],
+      ),
+        // SizedBox(height: 10),
+        Text(
+          '$ipaText',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        // SizedBox(height: 10),
+        // SizedBox(height: 10),
+        Text(
+          'Terjemahan: $translationText',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    flutterTts.stop();
+    super.dispose();
+  }
 }
+
 
 class QRScannerPage extends StatefulWidget {
   @override
@@ -233,9 +500,23 @@ class QRScannerPageState extends State<QRScannerPage> {
   }
 
   void _handleScannedResult(String scannedData, BuildContext context) {
-    // Assuming the scanned data contains the letter, e.g., "B" for consonants
-    bool isConsonant = RegExp(r'[BCDFGHJKLMNPQRSTVWXYZ]').hasMatch(scannedData);
-    context.read<MyAppState>().navigateToAlphabetPage(context, scannedData, isConsonant);
+    String category;
+
+    // Determine the category based on the scanned data (e.g., letters or QR content)
+    if (RegExp(r'[BCDFGHJKLMNPQRSTVWXYZ]').hasMatch(scannedData)) {
+      category = 'Consonants'; // If it's a consonant
+    } else if (RegExp(r'[AEIOUY]').hasMatch(scannedData)) {
+      category = 'Vowels'; // If it's a vowel
+    } else if (RegExp(r'[SemiConsonantPattern]').hasMatch(scannedData)) {
+      category = 'Semi-Consonants'; // Define the pattern for semi-consonants
+    } else {
+      category = 'Vokal Oral'; // You can add more conditions for Vokal Oral
+    }
+
+    // Navigate to the alphabet page with the scanned letter and category
+    context
+        .read<MyAppState>()
+        .navigateToAlphabetPage(context, scannedData, category);
   }
 
   @override
